13b99f4cdcfe49ec8ea10613b6906f3d7ab7515e
randysetiawanh <randysetiawanh@gmail.com>
2025-05-15 11:38:46 +0700

    Fix global crypto

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+global.crypto = require('crypto');
 const logger = require('./utils/logger');
 const startAbsenScheduler = require('./services/absenScheduler');
 const startEmasWatcher = require('./services/emasWatcher');
